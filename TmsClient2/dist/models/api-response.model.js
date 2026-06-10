6b7c5526d15e71e43adcbba3792d143087cff5f2
kalu <kaluabiyu@gmail.com>
2026-06-10 10:49:37 +0300

    delete Tsclient files

--- TmsClient2/dist/models/api-response.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export function renderResponse<T>(
-// response: ApiResponse<T>,
-// formatter: (data: T) => string,
-// ): string {
-// switch (){
-// }
-// }
